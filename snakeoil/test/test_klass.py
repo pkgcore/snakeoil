fb7766dc32aea539d6819dae5a667991181d6aeb
Brian Harring <ferringb@gmail.com>
2007-03-22 14:10:01 -0700

    pull in the updated test_klass from pkgcore, drop module mangling instead using currying.

--- snakeoil/test/test_klass.py
+++ snakeoil/test/test_klass.py
@@ -1,8 +1,8 @@
-# Copyright: 2006 Brian Harring <ferringb@gmail.com>
+# Copyright: 2006-2007 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
 from snakeoil.test import TestCase
-from snakeoil import klass
+from snakeoil import klass, currying
 
 
 class Test_native_GetAttrProxy(TestCase):
@@ -26,6 +26,18 @@ class Test_native_GetAttrProxy(TestCase):
         o.foon = "foo"
         self.assertEqual(o.foon, 'foo')
 
+    def test_attrlist(self):
+        def make_class(attr_list=None):
+            class foo(object):
+                __metaclass__ = self.kls
+            
+                if attr_list is not None:
+                    locals()['__attr_comparison__'] = attr_list
+        
+        self.assertRaises(TypeError, make_class)
+        self.assertRaises(TypeError, make_class, [u'foon'])
+        self.assertRaises(TypeError, make_class, [None])
+            
 
 class Test_CPY_GetAttrProxy(Test_native_GetAttrProxy):
 
@@ -85,48 +97,88 @@ class Test_CPY_get(Test_native_get):
     if klass.get is klass.native_get:
         skip = "cpython extension isn't available"
 
-def protect_eq_ops(method):
-    def f2(self, *args):
-        orig_eq = klass.generic_eq
-        orig_ne = klass.generic_ne
-        try:
-            klass.generic_eq = getattr(klass, self.op_prefix + "generic_eq")
-            klass.generic_ne = getattr(klass, self.op_prefix + "generic_ne")
-            method(self, *args)
-        finally:
-            klass.generic_eq = orig_eq
-            klass.generic_ne = orig_ne
-    return f2
-
 class Test_native_generic_equality(TestCase):
     op_prefix = "native_"
 
-    @protect_eq_ops
+    kls = currying.partial(klass.generic_equality,
+        ne=klass.native_generic_ne, eq=klass.native_generic_eq)
+
     def test_it(self):
         class c(object):
             __attr_comparison__ = ("foo", "bar")
-            __metaclass__ = klass.generic_equality
+            __metaclass__ = self.kls
             def __init__(self, foo, bar):
                 self.foo, self.bar = foo, bar
+            
+            def __repr__(self):
+                return "<c: foo=%r, bar=%r, %i>" % (
+                    getattr(self, 'foo', 'unset'),
+                    getattr(self, 'bar', 'unset'),
+                    id(self))
 
         self.assertEqual(c(1, 2), c(1, 2))
         c1 = c(1, 3)
         self.assertEqual(c1, c1)
         del c1
-        self.assertNotEqual(c(2, 1), c(1, 2))
+        self.assertNotEqual(c(2,1), c(1,2))
         c1 = c(1, 2)
         del c1.foo
-        self.assertNotEqual(c(1, 2), c1)
-        del c1
+        c2 = c(1, 2)
+        self.assertNotEqual(c1, c2)
+        del c2.foo
+        self.assertEqual(c1, c2)
 
-    @protect_eq_ops
     def test_call(self):
         def mk_class(meta):
             class c(object):
                 __metaclass__ = meta
-        self.assertRaises(TypeError, mk_class, klass.generic_equality)
+            return c
+        self.assertRaises(TypeError, mk_class)
+
 
 class Test_cpy_generic_equality(Test_native_generic_equality):
     op_prefix = ''
     if klass.native_generic_eq is klass.generic_eq:
         skip = "extension not available"
+
+    kls = staticmethod(klass.generic_equality)
+
+
+class Test_chained_getter(TestCase):
+
+    kls = klass.chained_getter
+    
+    def test_hash(self):
+        self.assertEqual(hash(self.kls("foon")), hash("foon"))
+        self.assertEqual(hash(self.kls("foon.dar")), hash("foon.dar"))
+
+    def test_caching(self):
+        l = []
+        for x in "abcdefghij":
+            l.append(id(self.kls("fa2341f%s" % x)))
+        self.assertEqual(id(self.kls("fa2341fa")), l[0])
+
+    def test_eq(self):
+        self.assertEqual(self.kls("asdf", disable_inst_caching=True),
+            self.kls("asdf", disable_inst_caching=True))
+
+        self.assertNotEqual(self.kls("asdf2", disable_inst_caching=True),
+            self.kls("asdf", disable_inst_caching=True))
+            
+    def test_it(self):
+        class maze(object):
+            def __init__(self, kwargs):
+                self.__data__ = kwargs
+                
+            def __getattr__(self, attr):
+                return self.__data__.get(attr, self)
+
+        d = {}
+        m = maze(d)
+        f = self.kls
+        self.assertEqual(f('foon')(m), m)
+        d["foon"] = 1
+        self.assertEqual(f('foon')(m), 1)
+        self.assertEqual(f('dar.foon')(m), 1)
+        self.assertEqual(f('.'.join(['blah']*10))(m), m)
+        self.assertRaises(AttributeError, f('foon.dar'), m)
