c8bd0362b3d7a186359f908e3254db23241ad5f3
Brian Harring <ferringb@gmail.com>
2007-04-12 20:58:54 -0700

    for lack of a better spot to stick it atm, add a basic loader to test_demandload_usage that scans the target source for locations to verify; via this, bypasses the issue of "if it's tested, it gets checked"

--- snakeoil/test/test_demandload_usage.py
+++ snakeoil/test/test_demandload_usage.py
@@ -1,26 +1,21 @@
 # Copyright: 2005 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
-import sys
+import os, stat
 
 from snakeoil.test import TestCase
 
 class TestDemandLoadTargets(TestCase):
 
-    def test_demandload_targets(self):
-        # picks up only the namespace loaded for searching
-        remaining = sys.modules.items()
-        seen = set()
-        while remaining:
-            for name, mod in remaining:
-                self.check_space(name, mod)
-                seen.add(name)
-            remaining = [
-                (k, v) for k, v in sys.modules.iteritems() if k not in seen]
+    valid_inits = frozenset("__init__.%s" % x for x in ("py", "pyc", "pyo", "so"))
 
-    def check_space(self, name, mod):
-        if not name.startswith("snakeoil."):
-            return
+    target_namespace = 'snakeoil'
+
+    def test_demandload_targets(self):
+        for x in self.get_modules(self.target_namespace):
+            self.check_space(x)
+
+    def check_space(self, mod):
         for attr in dir(mod):
             try:
                 obj = getattr(mod, attr)
@@ -28,4 +23,49 @@ class TestDemandLoadTargets(TestCase):
                 getattr(obj, "__class__", None)
             except ImportError:
                 # hit one.
-                self.fail("failed 'touching' demandloaded %s.%s" % (name, attr))
+                self.fail("failed 'touching' demandloaded %s.%s" % (mod.__name__, attr))
+
+    def recurse(self, location, require_init=True):
+        l = os.listdir(location)
+        if require_init and not self.valid_inits.intersection(l):
+            return
+
+        stats = [(x, os.stat(os.path.join(location, x)).st_mode) for x in l]
+        seen = set(['__init__'])
+        for (x, st) in stats:
+            if stat.S_ISREG(st):
+                if (x.endswith(".py") or x.endswith(".pyc")
+                    or x.endswith(".pyo") or x.endswith(".so")):
+                    y = x.rsplit(".", 1)[0]
+                    if y not in seen:
+                        seen.add(y)
+                        yield y
+
+        for (x, st) in stats:
+            if stat.S_ISDIR(st):
+                for y in self.recurse(os.path.join(location, x)):
+                    yield "%s.%s" % (x, y)
+
+    @staticmethod
+    def poor_mans_load(namespace):
+        return reduce(getattr, namespace.split(".")[1:], __import__(namespace))
+
+    def get_modules(self, namespace, ignore_failed_imports=False):
+        try:
+            i = self.poor_mans_load(namespace)
+            yield i
+        except ImportError:
+            if ignore_failed_imports:
+                return
+            raise
+        fn = i.__file__
+        if not fn.rsplit(".", 1)[0].endswith("__init__"):
+            yield namespace
+            return
+        for x in self.recurse(os.path.abspath(os.path.dirname(i.__file__))):
+            try:
+                yield self.poor_mans_load("%s.%s" % (i.__name__, x))
+            except ImportError:
+                if ignore_failed_imports:
+                    return
+                raise
