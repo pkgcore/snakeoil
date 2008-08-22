834eb7cb92377147352794210962e9169026c80f
Brian Harring <ferringb@gmail.com>
2008-08-21 19:21:58 -0700

    refactor the hell out of dependant_methods; avoids creating nasty strong cycles thus avoiding confusing gc, thus getting properly collected

--- snakeoil/dependant_methods.py
+++ snakeoil/dependant_methods.py
@@ -1,4 +1,4 @@
-# Copyright: 2005-2006 Brian Harring <ferringb@gmail.com>
+# Copyright: 2005-2008 Brian Harring <ferringb@gmail.com>
 # License: GPL2
 
 """Metaclass to inject dependencies into method calls.
@@ -8,27 +8,36 @@ if b is called first.
 """
 
 from snakeoil.lists import iflatten_instance
-from snakeoil.currying import partial
+from snakeoil.currying import pre_curry
 
 __all__ = ["ForcedDepends"]
 
-def ensure_deps(self, name, *a, **kw):
-    ignore_deps = "ignore_deps" in kw
+def ensure_deps(cls_id, name, func, self, *a, **kw):
+    ignore_deps = kw.pop("ignore_deps", False)
+    if id(self.__class__) != cls_id:
+#        import pdb;pdb.set_trace()
+        # child class calling parent, or something similar. for cls_id
+        # don't fire the dependants, nor update state
+        return func(self, *a, **kw)
+
     if ignore_deps:
-        del kw["ignore_deps"]
         s = [name]
     else:
         s = yield_deps(self, self.stage_depends, name)
 
     r = True
+    if not hasattr(self, '_stage_state'):
+        self._stage_state = set()
     for dep in s:
+#        if dep == 'merge_metadata':
+#            import pdb;pdb.set_trace()
         if dep not in self._stage_state:
-            r = getattr(self, dep).raw_func(*a, **kw)
-            if r:
-                self._stage_state.add(dep)
-            else:
+            r = getattr(self, dep).sd_raw_func(self, *a, **kw)
+            if not r:
                 return r
+            self._stage_state.add(dep)
     return r
+
 
 def yield_deps(inst, d, k):
     # While at first glance this looks like should use expandable_chain,
@@ -54,6 +63,33 @@ def yield_deps(inst, d, k):
             s.pop(-1)
 
 
+def __wrap_stage_dependencies__(cls):
+    stage_depends = cls.stage_depends
+    # we use id instead of the cls itself to prevent strong ref issues.
+    cls_id = id(cls)
+    for x in set(x for x in iflatten_instance(stage_depends.iteritems()) if x):
+        try:
+            f = getattr(cls, x)
+        except AttributeError:
+            raise TypeError("class %r stage_depends specifies "
+                "%r, which doesn't exist" % (cls, x))
+        f2 = pre_curry(ensure_deps, cls_id, x, f)
+        f2.sd_raw_func = f
+        setattr(cls, x, f2)
+
+
+def __unwrap_stage_dependencies__(cls):
+    stage_depends = cls.stage_depends
+    for x in set(x for x in iflatten_instance(stage_depends.iteritems()) if x):
+        try:
+            f = getattr(cls, x)
+        except AttributeError:
+            raise TypeError("class %r stage_depends specifies "
+                "%r, which doesn't exist" % (cls, x))
+        f2 = getattr(f, 'sd_raw_func', x)
+        setattr(cls, x, getattr(f, 'sd_raw_func', f))
+
+
 class ForcedDepends(type):
     """
     Metaclass forcing methods to run in a certain order.
@@ -66,21 +102,16 @@ class ForcedDepends(type):
     U{pkgcore projects pkgcore.intefaces.format.build_base is an example consumer<http://pkgcore.org>}
     to look at for usage.
     """
-    def __call__(cls, *a, **kw):
-        o = super(ForcedDepends, cls).__call__(*a, **kw)
-        if not getattr(cls, "stage_depends"):
-            return o
 
-        if not hasattr(o, "_stage_state"):
-            o._stage_state = set()
+    def __new__(cls, name, bases, d):
+        obj = type.__new__(cls, name, bases,d)
+        if not hasattr(obj, 'stage_depends'):
+            obj.stage_depends = {}
+        for x in ("wrap", "unwrap"):
+            s = '__%s_stage_dependencies__' % x
+            if not hasattr(obj, s):
+                setattr(obj, s, classmethod(globals()[s]))
 
-        # wrap the funcs
-
-        for x in set(x for x in iflatten_instance(o.stage_depends.iteritems())
-                     if x):
-            f = getattr(o, x)
-            f2 = partial(ensure_deps, o, x)
-            f2.raw_func = f
-            setattr(o, x, f2)
-
-        return o
+        obj.__unwrap_stage_dependencies__()
+        obj.__wrap_stage_dependencies__()
+        return obj
