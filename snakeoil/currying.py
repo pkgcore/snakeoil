d713e022d2a1d97d9d3678cc743a62698e8cd150
Brian Harring <ferringb@gmail.com>
2009-11-22 22:10:32 -0800

    fixup epydoc/pydoc generation; pull it into integration so that future api.pkgcore.org generations benefit

--- snakeoil/currying.py
+++ snakeoil/currying.py
@@ -32,6 +32,7 @@ while L{pre_curry} is python you should use L{partial} if possible.
 """
 
 from operator import attrgetter
+import sys
 
 __all__ = ("pre_curry", "partial", "post_curry", "pretty_docs",
     "alias_class_method")
@@ -81,6 +82,43 @@ except ImportError:
     except ImportError:
         partial = native_partial
 
+
+def _pydoc_isdata_override(object):
+    # yes, import in function is evil.
+    # so is this function...
+    import inspect
+    return not (isinstance(object, partial) or
+                inspect.ismodule(object) or inspect.isclass(object) or
+                inspect.isroutine(object) or inspect.isframe(object) or
+                inspect.istraceback(object) or inspect.iscode(object))
+
+
+def force_pydoc_partial_awareness(only_if_loaded=True):
+    if native_partial is not partial:
+        if only_if_loaded:
+            mod = sys.modules.get('pydoc', None)
+        else:
+            import pydoc as mod
+        if mod:
+            mod.isdata = _pydoc_isdata_override
+
+
+def _epydoc_isroutine_override(object):
+    # yes, import in function is evil.
+    # so is this function...
+    import inspect
+    return isinstance(object, partial) or inspect.isroutine(object)
+
+
+def force_epydoc_partial_awareness(only_if_loaded=True):
+    if native_partial is not partial:
+        if 'epydoc' in sys.modules or not only_if_loaded:
+            import epydoc.docintrospecter as mod
+            mod.register_introspecter(_epydoc_isroutine_override,
+                mod.introspect_routine, priority=26)
+
+force_pydoc_partial_awareness()
+force_epydoc_partial_awareness()
 
 def post_curry(func, *args, **kwargs):
     """passed in args are appended to any further args supplied"""
