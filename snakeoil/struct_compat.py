8e73ffa87c5f67b25c478c8e2ee5517694b4e49b
Brian Harring <ferringb@gmail.com>
2009-12-27 06:04:00 -0800

    snakeoil 0.3.5

--- snakeoil/struct_compat.py
+++ snakeoil/struct_compat.py
@@ -1,23 +1,21 @@
 # Copyright: 2009 Brian Harring <ferringb@gmail.com>
 # License: GPL2/BSD
 
-#import struct
 from struct import *
 
 class fake_struct(object):
 
     def __init__(self, format):
         self.format = format
-        self.size = struct.calcsize(self.format)
+        self.size = calcsize(self.format)
 
     def unpack(self, string):
-        return struct.unpack(self.format, string)
+        return unpack(self.format, string)
 
     def pack(self, *args):
-        return struct.pack(self.format, *args)
+        return pack(self.format, *args)
 
 base_struct = locals().get('Struct', fake_struct)
-#error = struct.error
 
 class Struct(base_struct):
 
--- snakeoil/version.py
+++ snakeoil/version.py
@@ -6,7 +6,7 @@
 
 import os
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 _ver = None
 
