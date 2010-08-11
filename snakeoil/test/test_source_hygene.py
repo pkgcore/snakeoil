4dafaadcfc72ab77c6734c2c6b4374123d1b8fe4
Brian Harring <ferringb@gmail.com>
2010-08-11 05:41:36 -0700

    pull in some cleanups for source hygene

--- snakeoil/test/test_source_hygene.py
+++ snakeoil/test/test_source_hygene.py
@@ -2,12 +2,15 @@
 # License: GPL2/BSD 3 clause
 
 from snakeoil.test import TestCase, mixins
+import sys
 
 class Test_modules(mixins.PythonNamespaceWalker, TestCase):
 
     target_namespace = 'snakeoil'
+    if sys.version_info[:2] <= (3,0):
+        module_blacklist = frozenset(["snakeoil.caching_2to3"])
 
-    def test_it(self):
+    def test__all__accuracy(self):
         failures = []
         for module in self.walk_namespace(self.target_namespace):
             for target in getattr(module, '__all__', ()):
