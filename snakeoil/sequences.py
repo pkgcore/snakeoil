2f8897ec622e060f2daec83c5322579319d37722
Brian Harring <ferringb@gmail.com>
2010-12-28 03:51:58 -0800

    add an initial shift to sequences instead of .lists; finally, add a namedtuple implementation

---
+++ snakeoil/sequences.py
@@ -0,0 +1,31 @@
+# Copyright: 2010 Brian Harring <ferringb@gmail.com>
+# License: GPL2/BSD 3 clause
+
+from snakeoil.lists import *
+from operator import itemgetter
+
+class base_namedtuple(tuple):
+
+    __slots__ = ()
+    _fields = ()
+
+    def __new__(cls, *values):
+        return super(base_namedtuple, cls).__new__(cls, values)
+
+def namedtuple(typename, field_names):
+    """Returns a new subclass of tuple with named fields.
+
+    while collections.namedtuple exists... it's fairly heavy and nasty in innards.
+
+    We choose to use a simpler version.
+    """
+
+    class kls(base_namedtuple):
+        __slots__ = ()
+        _fields = tuple(field_names)
+
+        locals().update((k, property(itemgetter(idx)))
+            for idx, k in enumerate(field_names))
+
+    kls.__name__ = typename
+    return kls
