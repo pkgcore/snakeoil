fec516d7430bb216f9cd948c3f476467d5ff6aec
Brian Harring <ferringb@gmail.com>
2012-01-19 04:44:22 -0800

    add basic api for getting # of actual non-HT cores.
    If it can't figure it out, just returns the # of procs it can find.

--- snakeoil/process/__init__.py
+++ snakeoil/process/__init__.py
@@ -7,16 +7,49 @@ from snakeoil.demandload import demandload
 demandload(globals(),
     'subprocess',
     'snakeoil.osutils:access',
+    'snakeoil.fileutils:readlines_ascii',
 )
+
+def _parse_cpuinfo():
+    data = readlines_ascii("/proc/cpuinfo", True, True, False)
+    procs = []
+    current = []
+    for line in data:
+        if not line:
+            if current:
+                procs.append(current)
+                current = []
+        else:
+            current.append(line.split(":", 1))
+    return [dict((k.strip(), v.strip()) for k,v in items)
+        for items in procs]
+
+def _get_linux_physical_proc_count():
+    procs = _parse_cpuinfo()
+    if not procs:
+        return _get_linux_proc_count()
+    core_count = {}
+    for proc in procs:
+        physical_id = proc.get('physical id')
+        if physical_id is None:
+            return _get_linux_proc_count()
+        if physical_id in core_count:
+            continue
+        cores = proc.get('cpu cores')
+        if cores is None:
+            return _get_linux_proc_count()
+        core_count[physical_id] = int(cores)
+
+    return sum(core_count.itervalues())
 
 def _get_linux_proc_count():
     try:
         return os.sysconf('SC_NPROCESSORS_ONLN')
     except (ValueError, OSError, AttributeError):
-        try:
-            return len([x for x in open("/proc/cpuinfo") if x.split(":", 1)[0].strip() == "processor"])
-        except EnvironmentError:
+        procs = _parse_cpuinfo()
+        if not procs:
             return None
+        return len(procs)
 
 def _get_bsd_proc_count():
     p = subprocess.Popen(["sysctl", "-n", "hw.cpu"],
@@ -30,6 +63,20 @@ def _get_bsd_proc_count():
             pass
     return None
 
+def get_physical_proc_count(force=False):
+    """return the number of non-HT cpu's identified
+
+    :param force: force recalculating the value, else use the cached value
+    :return: integer of the number of processors.  If it can't be discerned, 1 is returned
+    """
+    val = getattr(get_physical_proc_count, 'cached_result', None)
+    if val is None or force:
+        if 'linux' in sys.platform:
+            val = _get_linux_physical_proc_count()
+        else:
+            val = get_proc_count()
+        get_physical_proc_count.cached_value = val
+    return val
 
 def get_proc_count(force=False):
     """return the number of cpu's identified, HT or otherwise
