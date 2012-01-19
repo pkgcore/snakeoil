674c611f4f6debd28d8d3eb877bc8c7956ef6816
Brian Harring <ferringb@gmail.com>
2012-01-19 03:23:03 -0800

    shift native readfile/readlines into a location accessible for the extension
    This is needed since there are certain cases where an fs encountered has broken
    stat results; in those cases, the extensions will just defer to the slower native
    implementation since it's simpler code.

--- snakeoil/_fileutils.py
+++ snakeoil/_fileutils.py
@@ -7,10 +7,17 @@ compatibility module to break an import cycle, do not directly use this
 Access this functionality from :py:module:`snakeoil.osutils` instead
 """
 
-__all__ = ("mmap_and_close",)
+__all__ = ("mmap_and_close", "readlines_iter", "native_readlines",
+    "native_readfile")
 
 import os
 import mmap
+import itertools
+import errno
+from snakeoil import compatibility, demandload
+demandload.demandload(globals(),
+    'codecs',
+)
 
 def mmap_and_close(fd, *args, **kwargs):
     """
@@ -24,3 +31,118 @@ def mmap_and_close(fd, *args, **kwargs):
             os.close(fd)
         except EnvironmentError:
             pass
+
+
+class readlines_iter(object):
+    __slots__ = ("iterable", "mtime", "source")
+    def __init__(self, iterable, mtime, close=True, source=None):
+        if source is None:
+            source = iterable
+        self.source = source
+        if close:
+            iterable = itertools.chain(iterable, self._close_on_stop())
+        self.iterable = iterable
+        self.mtime = mtime
+
+    def _close_on_stop(self):
+        # we explicitly write this to force this method to be
+        # a generator; we intend to return nothing, but close
+        # the file on the way out.
+        for x in ():
+            # have to yield /something/, else py2.4 pukes on the syntax
+            yield None
+        self.source.close()
+        raise StopIteration()
+
+    def close(self):
+        if hasattr(self.source, 'close'):
+            self.source.close()
+
+    def __iter__(self):
+        return self.iterable
+
+
+def native_readlines(mode, mypath, strip_whitespace=True, swallow_missing=False,
+    none_on_missing=False, encoding=None, strict=compatibility.is_py3k):
+    """
+    read a file, yielding each line
+
+    :param mypath: fs path for the file to read
+    :param strip_whitespace: strip any leading or trailing whitespace including newline?
+    :param swallow_missing: throw an IOError if missing, or swallow it?
+    :param none_on_missing: if the file is missing, return None, else
+        if the file is missing return an empty iterable
+    """
+    handle = iterable = None
+    try:
+        if encoding and strict:
+            # we special case this- codecs.open is about 2x slower,
+            # thus if py3k, use the native one (which supports encoding directly)
+            if compatibility.is_py3k:
+                handle = open(mypath, mode, encoding=encoding)
+            else:
+                handle = codecs.open(mypath, mode, encoding=encoding)
+                if encoding == 'ascii':
+                    iterable = _py2k_ascii_strict_filter(handle)
+        else:
+            handle = open(mypath, mode)
+    except IOError, ie:
+        if not swallow_missing or ie.errno not in (errno.ENOTDIR, errno.ENOENT):
+            raise
+        if none_on_missing:
+            return None
+        return readlines_iter(iter([]), None, close=False)
+
+    mtime = os.fstat(handle.fileno()).st_mtime
+    if not iterable:
+        iterable = iter(handle)
+    if not strip_whitespace:
+        return readlines_iter(iterable, mtime)
+    return readlines_iter(_strip_whitespace_filter(iterable), mtime,
+        source=handle)
+
+
+def _strip_whitespace_filter(iterable):
+    for line in iterable:
+        yield line.strip()
+
+
+def _py2k_ascii_strict_filter(source):
+    any = compatibility.any
+    for line in source:
+        if any((0x80 & ord(char)) for char in line):
+            raise ValueError("character ordinal over 127");
+        yield line
+
+
+def native_readfile(mode, mypath, none_on_missing=False, encoding=None,
+    strict=compatibility.is_py3k):
+    """
+    read a file, returning the contents
+
+    :param mypath: fs path for the file to read
+    :param none_on_missing: whether to return None if the file is missing,
+        else through the exception
+    """
+    f = None
+    try:
+        try:
+            if encoding and strict:
+                # we special case this- codecs.open is about 2x slower,
+                # thus if py3k, use the native one (which supports encoding directly)
+                if compatibility.is_py3k:
+                    f = open(mypath, mode, encoding=encoding)
+                else:
+                    f = codecs.open(mypath, mode, encoding=encoding)
+            else:
+                f = open(mypath, mode)
+
+            return f.read()
+        except IOError, oe:
+            if none_on_missing and oe.errno in (errno.ENOENT, errno.ENOTDIR):
+                return None
+            raise
+    finally:
+        if f is not None:
+            f.close()
+
--- snakeoil/fileutils.py
+++ snakeoil/fileutils.py
@@ -239,44 +239,13 @@ class ParseError(Exception):
         self.file, self.line, self.errmsg = filename, line, errmsg
 
 
-def _internal_native_readfile(mode, mypath, none_on_missing=False, encoding=None,
-    strict=compatibility.is_py3k):
-    """
-    read a file, returning the contents
-
-    :param mypath: fs path for the file to read
-    :param none_on_missing: whether to return None if the file is missing,
-        else through the exception
-    """
-    f = None
-    try:
-        try:
-            if encoding and strict:
-                # we special case this- codecs.open is about 2x slower,
-                # thus if py3k, use the native one (which supports encoding directly)
-                if compatibility.is_py3k:
-                    f = open(mypath, mode, encoding=encoding)
-                else:
-                    f = codecs.open(mypath, mode, encoding=encoding)
-            else:
-                f = open(mypath, mode)
-
-            return f.read()
-        except IOError, oe:
-            if none_on_missing and oe.errno in (errno.ENOENT, errno.ENOTDIR):
-                return None
-            raise
-    finally:
-        if f is not None:
-            f.close()
-
 def _mk_pretty_derived_func(func, name_base, name, *args, **kwds):
     if name:
         name = '_' + name
     return pretty_docs(partial(func, *args, **kwds),
         name='%s%s' % (name_base, name))
 
-_mk_readfile = partial(_mk_pretty_derived_func, _internal_native_readfile,
+_mk_readfile = partial(_mk_pretty_derived_func, _fileutils.native_readfile,
     'readfile')
 
 native_readfile_ascii = _mk_readfile('ascii', 'rt')
@@ -289,87 +258,8 @@ native_readfile_utf8 = _mk_readfile('utf8', 'r',
 native_readfile_utf8_strict = _mk_readfile('utf8_strict', 'r',
     encoding='utf8', strict=True)
 
-class readlines_iter(object):
-    __slots__ = ("iterable", "mtime", "source")
-    def __init__(self, iterable, mtime, close=True, source=None):
-        if source is None:
-            source = iterable
-        self.source = source
-        if close:
-            iterable = itertools.chain(iterable, self._close_on_stop())
-        self.iterable = iterable
-        self.mtime = mtime
 
-    def _close_on_stop(self):
-        # we explicitly write this to force this method to be
-        # a generator; we intend to return nothing, but close
-        # the file on the way out.
-        for x in ():
-            # have to yield /something/, else py2.4 pukes on the syntax
-            yield None
-        self.source.close()
-        raise StopIteration()
-
-    def close(self):
-        if hasattr(self.source, 'close'):
-            self.source.close()
-
-    def __iter__(self):
-        return self.iterable
-
-def _py2k_ascii_strict_filter(source):
-    any = compatibility.any
-    for line in source:
-        if any((0x80 & ord(char)) for char in line):
-            raise ValueError("character ordinal over 127");
-        yield line
-
-
-def _strip_whitespace_filter(iterable):
-    for line in iterable:
-        yield line.strip()
-
-def native_readlines(mode, mypath, strip_whitespace=True, swallow_missing=False,
-    none_on_missing=False, encoding=None, strict=compatibility.is_py3k):
-    """
-    read a file, yielding each line
-
-    :param mypath: fs path for the file to read
-    :param strip_whitespace: strip any leading or trailing whitespace including newline?
-    :param swallow_missing: throw an IOError if missing, or swallow it?
-    :param none_on_missing: if the file is missing, return None, else
-        if the file is missing return an empty iterable
-    """
-    handle = iterable = None
-    try:
-        if encoding and strict:
-            # we special case this- codecs.open is about 2x slower,
-            # thus if py3k, use the native one (which supports encoding directly)
-            if compatibility.is_py3k:
-                handle = open(mypath, mode, encoding=encoding)
-            else:
-                handle = codecs.open(mypath, mode, encoding=encoding)
-                if encoding == 'ascii':
-                    iterable = _py2k_ascii_strict_filter(handle)
-        else:
-            handle = open(mypath, mode)
-    except IOError, ie:
-        if not swallow_missing or ie.errno not in (errno.ENOTDIR, errno.ENOENT):
-            raise
-        if none_on_missing:
-            return None
-        return readlines_iter(iter([]), None, close=False)
-
-    mtime = os.fstat(handle.fileno()).st_mtime
-    if not iterable:
-        iterable = iter(handle)
-    if not strip_whitespace:
-        return readlines_iter(iterable, mtime)
-    return readlines_iter(_strip_whitespace_filter(iterable), mtime,
-        source=handle)
-
-
-_mk_readlines = partial(_mk_pretty_derived_func, native_readlines,
+_mk_readlines = partial(_mk_pretty_derived_func, _fileutils.native_readlines,
     'readlines')
 
 try:
