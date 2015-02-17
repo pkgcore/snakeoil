7a63877e685c5ad3ff9ae4aea6405c7d4d792313
Tim Harder <radhermit@gmail.com>
2015-02-16 20:02:14 -0800

    process: add get_exit_status and exit_as_status

--- snakeoil/process/__init__.py
+++ snakeoil/process/__init__.py
@@ -1,12 +1,19 @@
 # Copyright: 2011-2012 Brian Harring <ferringb@gmail.com>
+# Copyright 2014 The Chromium OS Authors. All rights reserved. (get_exit_status and exit_as_status)
 # License: GPL2/BSD 3 clause
+
+"""Process related utilities."""
 
 import os
 import sys
 
 from snakeoil.demandload import demandload
+
 demandload(
+    'errno',
+    'signal',
     'subprocess',
+    'time',
     'snakeoil.osutils:access',
     'snakeoil.fileutils:readlines_ascii',
 )
@@ -114,6 +121,59 @@ def find_binary(binary, paths=None):
             return filename
 
     raise CommandNotFound(binary)
+
+
+def get_exit_status(status):
+    """Get the exit status of a child from an os.waitpid call.
+
+    Args:
+        status: The return value of os.waitpid(pid, 0)[1]
+
+    Returns:
+        The exit status of the process. If the process exited with a signal,
+        the return value will be 128 plus the signal number.
+    """
+    if os.WIFSIGNALED(status):
+        return 128 + os.WTERMSIG(status)
+    else:
+        assert os.WIFEXITED(status), 'Unexpected exit status %r' % status
+        return os.WEXITSTATUS(status)
+
+
+def exit_as_status(status):
+    """Exit the same way as |status|.
+
+    If the status field says it was killed by a signal, then we'll do that to
+    ourselves.  Otherwise we'll exit with the exit code.
+
+    See http://www.cons.org/cracauer/sigint.html for more details.
+
+    Args:
+        status: A status as returned by os.wait type funcs.
+    """
+    exit_status = os.WEXITSTATUS(status)
+
+    if os.WIFSIGNALED(status):
+        # Kill ourselves with the same signal.
+        sig_status = os.WTERMSIG(status)
+        pid = os.getpid()
+        os.kill(pid, sig_status)
+        time.sleep(0.1)
+
+        # Still here?  Maybe the signal was masked.
+        try:
+            signal.signal(sig_status, signal.SIG_DFL)
+        except RuntimeError as e:
+            if e.args[0] != errno.EINVAL:
+                raise
+        os.kill(pid, sig_status)
+        time.sleep(0.1)
+
+        # Still here?  Just exit.
+        exit_status = 127
+
+    # Exit with the code we want.
+    sys.exit(exit_status)
 
 
 class CommandNotFound(Exception):
