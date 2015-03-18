8a13deeb87e4e85499abc0159e6039ea347a956e
Brian Harring <ferringb@gmail.com>
2015-03-18 18:37:44 -0500

    snakeoil.version: don't fail if git is missing.
    If running from a tarball or a git checkout, snakeoil.version requires
    git to be available or it throws an exception from subprocess.
    
    Instead, it should suppress that and just return unknown date/tag/version.

--- snakeoil/version.py
+++ snakeoil/version.py
@@ -8,17 +8,19 @@
 __version__ = '0.6.1'
 
 _ver = None
+import errno
 import os
 
-def _run(cwd, cmd):
+def _run_git(cwd, cmd):
     import subprocess
 
     env = dict(os.environ)
     env["LC_CTYPE"] = "C"
 
     with open(os.devnull, 'wb') as null:
-        r = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env,
-                             stderr=null, cwd=cwd)
+        r = subprocess.Popen(
+            ['git'] + list(cmd), stdout=subprocess.PIPE, env=env,
+            stderr=null, cwd=cwd)
 
     stdout = r.communicate()[0]
     return stdout, r.returncode
@@ -28,18 +30,24 @@ def get_git_version(cwd):
     """:return: git sha1 rev"""
 
     cwd = os.path.abspath(cwd)
-    stdout, ret = _run(cwd, ["git", "log", "--format=%H\n%ad", "HEAD^..HEAD"])
+    try:
+        stdout, ret = _run_git(cwd, ["log", "--format=%H\n%ad", "HEAD^..HEAD"])
 
-    if ret != 0:
-        return {}
+        if ret != 0:
+         return {}
 
-    data = stdout.decode("ascii").splitlines()
+        data = stdout.decode("ascii").splitlines()
 
-    return {"rev": data[0], "date": data[1], 'tag': _get_git_tag(cwd, data[0])}
+        return {"rev": data[0], "date": data[1], 'tag': _get_git_tag(cwd, data[0])}
+    except EnvironmentError, e:
+        # ENOENT is thrown when the git binary can't be found.
+        if e.errno != errno.ENOENT:
+            raise
+        return {'rev':'unknown', 'date':'unknown', 'tag':'unknown'}
 
 
 def _get_git_tag(cwd, rev):
-    stdout, _ret = _run(cwd, ['git', 'name-rev', '--tag', rev])
+    stdout, _ret = _run_git(cwd, ['name-rev', '--tag', rev])
     tag = stdout.decode("ascii").split()
     if len(tag) != 2:
         return None
