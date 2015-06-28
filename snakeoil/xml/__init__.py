3844b2ae30ace5259387b4851a877fcba5e6a670
Tim Harder <radhermit@gmail.com>
2015-06-27 21:40:43 -0400

    vastly simplify snakeoil.xml
    Drop most of the fallbacks from trying to find a compatible elementtree
    module. Just check for the C implementation and then fall back to the
    native python implementation as necessary. In python3, using
    cElementTree is deprecated as well so this module can be removed once we
    drop python2 support.
    
    Also, instead of our own internal method the escape method from
    xml.sax.saxutils can be used.

--- snakeoil/xml/__init__.py
+++ snakeoil/xml/__init__.py
@@ -2,57 +2,11 @@
 # License: BSD/GPL2
 
 """
-Compatibility code, preferring cElementTree, falling back as necessary.
+Compatibility code, preferring cElementTree, falling back to ElementTree.
 """
-# essentially... prefer cElementTree, then 2.5 bundled, then
-# elementtree, then 2.5 bundled, then our own bundled
 
-# "No name etree in module xml", "Reimport cElementTree"
-# pylint: disable=E0611,W0404
-
-etree = None
 try:
     # pylint: disable=import-error
-    import cElementTree as etree
+    from xml.etree import cElementTree as etree
 except ImportError:
-    pass
-
-if etree is None:
-    try:
-        # pylint: disable=import-error
-        from xml.etree import cElementTree as etree
-        if not hasattr(etree, 'parse'):
-            etree = None
-    except ImportError:
-        pass
-
-if etree is None:
-    try:
-        # pylint: disable=import-error
-        from elementtree import ElementTree as etree
-    except ImportError:
-        pass
-
-if etree is None:
-    try:
-        # pylint: disable=import-error
-        from xml.etree import cElementTree as etree
-        if not hasattr(etree, 'parse'):
-            etree = None
-    except ImportError:
-        pass
-
-if etree is None:
-    try:
-        # pylint: disable=import-error
-        from xml.etree import ElementTree as etree
-    except ImportError:
-        pass
-
-
-def escape(string):
-    """
-    simple escaping of &, <, and >
-    """
-    return string.replace("&", "&amp;").replace("<", "&lt;").replace(">",
-                                                                     "&gt;")
+    from xml.etree import ElementTree as etree
