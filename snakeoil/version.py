4dcfe5ef92ff38f0ec323de53fcd9048562b17cb
Tim Harder <radhermit@gmail.com>
2015-07-23 18:14:49 -0400

    version: fix get_version() for usage in git repos

--- snakeoil/version.py
+++ snakeoil/version.py
@@ -9,7 +9,7 @@ import os
 _ver = None
 
 
-def get_version(project, file_in_the_repo):
+def get_version(project, repo_file):
     """:return: a string describing the project version."""
     global _ver  # pylint: disable=global-statement
     if _ver is None:
@@ -18,11 +18,11 @@ def get_version(project, file_in_the_repo):
         try:
             version_info = getattr(import_module(
                 '%s._verinfo' % (project,)), 'version_info')
-        except AttributeError:
+        except ImportError:
             # we're probably in a git repo
             try:
-                from pkgdist import get_git_version
-                cwd = os.path.dirname(os.path.abspath(file_in_the_repo))
+                from pkgdist.version import get_git_version
+                cwd = os.path.dirname(os.path.abspath(repo_file))
                 version_info = get_git_version(cwd)
             except ImportError:
                 pass
