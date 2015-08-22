95c56b8d298a95b40305ccf507b4b9829e92f0dc
Tim Harder <radhermit@gmail.com>
2015-08-21 22:11:39 -0400

    cli: add some more docs on how to use add_argument monkey patching

--- snakeoil/cli.py
+++ snakeoil/cli.py
@@ -1,6 +1,8 @@
 # Copyright: 2015 Tim Harder <radhermit@gmail.com>
 # License: BSD/GPL2
 
+"""Various command-line related support"""
+
 import argparse
 
 from snakeoil.klass import patch
@@ -14,8 +16,16 @@ _generate_docs = False
 def add_argument(argparse_add_argument, self, *args, **kwargs):
     """Enable docs keyword args support for arguments.
 
-    This is used to add extended, rST-formatted docs to man pages without
-    affecting the regular help output for scripts.
+    This is used to add extended, rST-formatted docs to man pages (or other
+    generated doc formats) without affecting the regular, summarized help
+    output for scripts.
+
+    To use, import this module where argparse is used to add command-line
+    arguments so the 'docs' kwarg gets ignored during general use. During
+    document generation, enable the global _generate_docs variable in order to
+    add 'docs' attributes to action objects that specify them. The strings from
+    those attributes can then be extracted and added to the correct locations
+    in the generated docs, see snakeoil.dist.generate_man_rsts for an example.
     """
     docs = kwargs.pop('docs', None)
     action = argparse_add_argument(self, *args, **kwargs)
