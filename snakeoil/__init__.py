eef59261518e622c8c7e599e37399d719b0a00af
Tim Harder <radhermit@gmail.com>
2015-11-26 19:28:57 -0500

    update supported python versions

--- snakeoil/__init__.py
+++ snakeoil/__init__.py
@@ -10,8 +10,9 @@ This library is a bit of a grabbag of the following:
 * standard lib fixups; a new style UserDict base class for example that
   is designed around iter* overriding, rather than sequence methods as
   UserDict is.
-* python version compatibility; snakeoil supports 2.5 through 3.2, exposing
-  fallback implementations of desirable functionality in older python versions.
+* python version compatibility; snakeoil supports 2.7 and 3.3 through 3.5,
+  exposing fallback implementations of desirable functionality in older python
+  versions.
 * optimized implementations of common patterns
 
 """
