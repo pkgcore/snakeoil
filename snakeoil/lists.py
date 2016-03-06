fc17088394a0c5ce060ea0b3247ecb5af2930457
Tim Harder <radhermit@gmail.com>
2016-03-05 21:02:45 -0500

    lists: re-add deprecated lists module stub until 0.8

---
+++ snakeoil/lists.py
@@ -0,0 +1,7 @@
+# TODO: remove in 0.8
+
+import warnings
+warnings.warn('snakeoil.lists is deprecated, use snakeoil.sequences', DeprecationWarning)
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+from snakeoil.sequences import *
