b773891c93a1a1a3a3875e44daa9220d2f91551a
Tim Harder <radhermit@gmail.com>
2017-09-27 03:58:22 -0400

    bump version

--- src/snakeoil/__init__.py
+++ src/snakeoil/__init__.py
@@ -10,11 +10,11 @@ This library is a bit of a grabbag of the following:
 * standard lib fixups; a new style UserDict base class for example that
   is designed around iter* overriding, rather than sequence methods as
   UserDict is.
-* python version compatibility; snakeoil supports 2.7 and 3.3 through 3.5,
+* python version compatibility; snakeoil supports 2.7 and 3.4 through 3.6,
   exposing fallback implementations of desirable functionality in older python
   versions.
 * optimized implementations of common patterns
 """
 
 __title__ = 'snakeoil'
-__version__ = '0.7.3'
+__version__ = '0.7.4'
