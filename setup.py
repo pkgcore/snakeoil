bfc0e2aa7870b6c9dc06d703b4a62442ad2833fb
Tim Harder <radhermit@gmail.com>
2019-11-06 21:54:39 -0700

    travis-ci: enable python 3.8 builds

--- setup.py
+++ setup.py
@@ -92,6 +92,7 @@ setup(**dict(pkgdist_setup,
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         ],
     )
 )
