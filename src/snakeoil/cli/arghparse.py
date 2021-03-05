95aa5b739cca5b8a780ce10a642d1551e074f5cc
Tim Harder <radhermit@gmail.com>
2021-03-05 03:44:02 -0700

    cli.arghparse: add support for nargs with StoreBool

--- src/snakeoil/cli/arghparse.py
+++ src/snakeoil/cli/arghparse.py
@@ -286,6 +286,7 @@ class StoreBool(argparse._StoreAction):
     def __init__(self,
                  option_strings,
                  dest,
+                 nargs=None,
                  const=None,
                  default=None,
                  required=False,
@@ -294,6 +295,7 @@ class StoreBool(argparse._StoreAction):
         super().__init__(
             option_strings=option_strings,
             dest=dest,
+            nargs=nargs,
             const=const,
             default=default,
             type=self.boolean,
