e3d87341a49253d68e09323321d314b8b1d76b4f
Brian Harring <ferringb@gmail.com>
2025-11-29 00:57:52 +0100

    chore: deprecated and gut eq_hash_inheritance.  It's tests for asserting py2k/py3k compatibility.
    The test has been a no-op for I've no clue how long, but presumably a long time.  Thus
    make it a noop'd class that has deprecation notices.
    
    Signed-off-by: Brian Harring <ferringb@gmail.com>

--- src/snakeoil/test/eq_hash_inheritance.py
+++ src/snakeoil/test/eq_hash_inheritance.py
@@ -1,46 +1,10 @@
-from .slot_shadowing import KlassWalker, TargetedNamespaceWalker
+__all__ = ("Test",)
+
+from ..deprecation import deprecated
 
 
-class Test(TargetedNamespaceWalker, KlassWalker):
-    target_namespace = "snakeoil"
-
-    singleton = object()
-
-    def setup_method(self):
-        self._ignore_set = frozenset(self.iter_builtin_targets())
-
-    def _should_ignore(self, cls):
-        if cls in self._ignore_set:
-            return True
-
-        if getattr(cls, "__hash__intentionally_disabled__", False):
-            return True
-
-        namepath = f"{cls.__module__}.{cls.__name__}"
-        return not namepath.startswith(self.target_namespace)
-
-    def run_check(self, cls):
-        for parent in cls.__bases__:
-            if parent is object:
-                # object sets __hash__/__eq__, which isn't usually
-                # intended to be inherited/reused
-                continue
-            eq = getattr(parent, "__eq__", self.singleton)
-            h = getattr(parent, "__hash__", self.singleton)
-            if eq == object.__eq__ and h == object.__hash__:
-                continue
-            if eq and h:
-                break
-        else:
-            return
-
-        # pylint: disable=undefined-loop-variable
-        # 'parent' is guaranteed to be defined due to the 'else' clause above
-        assert getattr(cls, "__hash__") is not None, (
-            f"class '{cls.__module__}.{cls.__name__}' had its __hash__ reset, "
-            "while it would've inherited __hash__ from parent "
-            f"'{parent.__module__}.{parent.__name__}'; this occurs in py3k when "
-            "__eq__ is  defined alone.  If this is desired behaviour, set "
-            "__hash__intentionally_disabled__ to True to explicitly ignore this"
-            " class"
-        )
+@deprecated(
+    "snakeoil.test.eq_hash_inheritance.Test is a noop.  Remove it from your tests"
+)
+class Test:
+    """Dead set of tests for asserting py2k/py3k compatibility"""
--- tests/test_eq_hash_inheritance.py
+++
@@ -1,5 +0,0 @@
-from snakeoil.test.eq_hash_inheritance import Test
-
-
-class Test_Eq_Hash(Test):
-    pass
