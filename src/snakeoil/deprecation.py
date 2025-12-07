bec83bf98b3d9618fb8e78f185d4838bdb14889b
Brian Harring <ferringb@gmail.com>
2025-12-07 22:10:30 +0100

    doc: tweak deprecation module doc to be a bit clearer.  Still feels opaque
    Signed-off-by: Brian Harring <ferringb@gmail.com>

--- src/snakeoil/deprecation.py
+++ src/snakeoil/deprecation.py
@@ -7,6 +7,11 @@ to be attached to the deprecation, and tracking all deprecations created
 by that registry.  This allows tests to do introspection for deprecations
 that can now be removed.
 
+To use this, instantiate a registry, and then use it to decorate functions
+(exactly like warnings.deprecated in py3.13).  This just keeps a record of
+them so that code analysis can be done for things that need to be removed
+when future conditions are met.
+
 """
 
 __all__ = ("Registry", "RecordCallable", "suppress_deprecations")
